0e2901e6f6aa2548646a1a64b202589e6610f502
Aryo-Wibisono <yoannes.deo@ti.ukdw.ac.id>
2024-12-26 18:21:35 +0700

    feature hash password and check already user

--- backend/model/accountModel.js
+++ backend/model/accountModel.js
@@ -1,56 +1,71 @@
-const db= require('../databases/config');
+const bcrypt = require('bcrypt');
+const db = require('../databases/config');
 
 const register = (data) => {
     const { username, password, payment_id, balance } = data;
 
-
+    const userAlreadyExists = `SELECT * FROM account WHERE name = ?`;
     const queryAccount = `INSERT INTO account (name, password, payment_id, balance) VALUES (?, ?, ?, ?)`;
-
-
     const queryWallet = `INSERT INTO wallet (payment_id, balance) VALUES (?, ?)`;
 
-
-    const valuesAccount = [username, password, payment_id, balance];
-    const valuesWallet = [payment_id, balance];
-
     return new Promise((resolve, reject) => {
-        // start transaction if errors in one of query then rollback
-        db.beginTransaction((err) => {
+        db.query(userAlreadyExists, [username], async (err, res) => {
             if (err) {
                 return reject(err);
             }
 
+            if (res.length > 0) {
+                resolve({
+                    message: `User already exists`
+                });
+            } else {
 
-            db.query(queryAccount, valuesAccount, (err, result) => {
-                if (err) {
-                    return db.rollback(() => {
-                        reject(err);
-                    });
-                }
+                const passwordHash = await bcrypt.hash(password, 10);
 
-                db.query(queryWallet, valuesWallet, (err, result) => {
+                const valuesAccount = [username, passwordHash, payment_id, balance];
+                const valuesWallet = [payment_id, balance];
+
+                // Mulai transaksi
+                db.beginTransaction((err) => {
                     if (err) {
-                        return db.rollback(() => {
-                            reject(err);
-                        });
+                        return reject(err);
                     }
 
-                    db.commit((err) => {
+                    // Query untuk akun
+                    db.query(queryAccount, valuesAccount, (err, result) => {
                         if (err) {
                             return db.rollback(() => {
                                 reject(err);
                             });
                         }
 
-                        resolve({
-                            message: 'Account created successfully.',
-                            accountName: username
+                        // Query untuk wallet
+                        db.query(queryWallet, valuesWallet, (err, result) => {
+                            if (err) {
+                                return db.rollback(() => {
+                                    reject(err);
+                                });
+                            }
+
+                            // Commit transaksi
+                            db.commit((err) => {
+                                if (err) {
+                                    return db.rollback(() => {
+                                        reject(err);
+                                    });
+                                }
+
+                                resolve({
+                                    message: 'Account created successfully.',
+                                    accountName: username
+                                });
+                            });
                         });
                     });
                 });
-            });
+            }
         });
     });
 };
 
-module.exports={register}
+module.exports = { register };
