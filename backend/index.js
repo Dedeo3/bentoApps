d57a2484f5f5896ac42ea5ede3f874724c23dabd
Aryo-Wibisono <yoannes.deo@ti.ukdw.ac.id>
2024-12-29 01:10:19 +0700

    update port implemented

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
 const  express= require('express')
 const app = express()
+require('dotenv').config();
+
 const handleMiddleware= require('cors')
 
 app.use(express.json())
 app.use(handleMiddleware())
 
 const account=require('./routes/account')
+const menu=require('./routes/menu')
 
 app.use('/account',account)
-app.listen(8181,()=>{
+app.use('/menu',menu)
+app.listen(process.env.PORT,()=>{
     console.log("Server started on port 8181")
 })
