a518c3d2c1bc36bf2585cdd5e86f07dd8c7cb8d7
Aryo-Wibisono <yoannes.deo@ti.ukdw.ac.id>
2024-12-29 14:20:06 +0700

    add sort by category feature

--- backend/routes/menu.js
+++ backend/routes/menu.js
@@ -5,6 +5,6 @@ const auth=require('../util/autentication')
 router.get('/listMenu',auth,controller.listMenu)
 router.post('/selectedMenu',auth,controller.selectedMenu)
 router.post('/search',auth,controller.search)
-router.post('/categoryMenu',auth,controller.search)
+router.post('/categoryMenu',auth,controller.category)
 
 module.exports = router
